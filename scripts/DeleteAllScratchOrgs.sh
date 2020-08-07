21d3bfa786cdfe4269d5f3e543678f2c09a13717
David Larrimore <davidlarrimore@gmail.com>
2020-08-07 19:57:37 -0400

    Commit

--- scripts/DeleteAllScratchOrgs.sh
+++ scripts/DeleteAllScratchOrgs.sh
@@ -3,4 +3,6 @@ sfdx force:data:soql:query -q "SELECT Id, Username FROM ScratchOrgInfo" -r csv -
 cat ./ScratchOrgs.csv
 echo "Deleting found scratch Orgs..."
 sfdx force:data:bulk:delete -f ./ScratchOrgs.csv -s ScratchOrgInfo -w 10 -u MyComponents
+echo "Cleaning up org list"
+sfdx force:org:list --clean
 rm -f ./ScratchOrgs.csv
