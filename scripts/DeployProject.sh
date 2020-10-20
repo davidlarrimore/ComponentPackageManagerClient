4cfae7995002a90c43a2e475e2c668f1b1903b48
Dave Larrimore <dlarrimore@salesforce.com>
2020-10-20 06:35:37 -0400

    brand new package, brand new permission set

--- scripts/DeployProject.sh
+++ scripts/DeployProject.sh
@@ -1,17 +1,22 @@
 
-#No Longer Needed
-#echo "*** Creating Managed Package ..."
-sfdx force:package:create --name "Demo Component Package Manager" --packagetype Unlocked --path "force-app"
-#sfdx force:package1:version:create --packageid 033xx00000007oi --name ”Spring 17” --description ”Spring 17 Release” --version 3.2 --managedreleased
+echo "*** Deploying Source to Org ..."
+sfdx force:source:deploy --targetusername MyComponents --sourcepath force-app
+
+echo "*** Run Test Cases W\Coverage ..."
+sfdx force:apex:test:run -c -u MyComponents -r human
 
 echo "*** Export Data..."
 #sfdx force:data:tree:export -q ./scripts/soql/demo.soql -x export-demo -d ./data/
 
-echo "*** Creating Managed Package Version..."
-sfdx force:package:version:create --package "Demo Component Package Manager" -x --wait 10 --codecoverage
+#No Longer Needed
+#echo "*** Creating Managed Package ..."
+#sfdx force:package:create -n "Component Package Manager" -t Unlocked -r force-app --targetdevhubusername MyComponents
 
-#echo "*** Promoting Latest Managed Package ..."
-sfdx force:package:version:promote -p $(sfdx force:package:version:list -p 'Demo Component Package Manager' -o CreatedDate --concise | tail -1 | awk '{print $3}')
+echo "*** Creating Managed Package Version..."
+sfdx force:package:version:create --package "Component Package Manager" -x --wait 10 --codecoverage  --definitionfile config/project-scratch-def.json
+
+echo "*** Promoting Latest Managed Package ..."
+sfdx force:package:version:promote -p $(sfdx force:package:version:list -p 'Component Package Manager' -o CreatedDate --concise | tail -1 | awk '{print $3}')
 
 #echo "*** Pushing Package to Package Manager Org ..."
-#sfdx force:package:install --package "Demo Reset Tools@0.1.0-6" --targetusername PackageManager --installationkey test1234
+#sfdx force:package:install --package $(sfdx force:package:version:list -p 'Component Package Manager' -o CreatedDate --concise | tail -1 | awk '{print $3}') --targetusername PackageManagerTesting
