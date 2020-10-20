a8d9fa68b2e0b77474d8dd94be252f9f8e1a7af1
Dave Larrimore <dlarrimore@salesforce.com>
2020-10-20 09:46:11 -0400

    Installable Version Created

--- scripts/DeployProject.sh
+++ scripts/DeployProject.sh
@@ -10,7 +10,7 @@ echo "*** Export Data..."
 
 #No Longer Needed
 #echo "*** Creating Managed Package ..."
-#sfdx force:package:create -n "Component Package Manager" -t Unlocked -r force-app --targetdevhubusername MyComponents
+#sfdx force:package:create - "Component Package Manager" -t Unlocked -r force-app --targetdevhubusername MyComponents
 
 echo "*** Creating Managed Package Version..."
 sfdx force:package:version:create --package "Component Package Manager" -x --wait 10 --codecoverage  --definitionfile config/project-scratch-def.json
@@ -19,4 +19,4 @@ echo "*** Promoting Latest Managed Package ..."
 sfdx force:package:version:promote -p $(sfdx force:package:version:list -p 'Component Package Manager' -o CreatedDate --concise | tail -1 | awk '{print $3}')
 
 #echo "*** Pushing Package to Package Manager Org ..."
-#sfdx force:package:install --package $(sfdx force:package:version:list -p 'Component Package Manager' -o CreatedDate --concise | tail -1 | awk '{print $3}') --targetusername PackageManagerTesting
+#sfdx force:package:install --package $(sfdx force:package:version:list -p 'Component Package Manager' -o CreatedDate --concise | tail -1 | awk '{print $3}') --targetusername PackageManagerTesting --apexcompile package
